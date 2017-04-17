af3eaf581afbee0f1fc8e7c6020ac0fa7e6a59ed
wayne0327 <wayne890327@gmail.com>
2017-04-17 11:47:24 +0800

    add button message

--- winform/Form1.cs
+++ winform/Form1.cs
@@ -26,5 +26,10 @@ namespace winform
         {
             label2.Text = textBox1.Text;
         }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("hello");
+        }
     }
 }
